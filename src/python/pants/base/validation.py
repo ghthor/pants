9e253703848ff6febfedaed9df17d9f041219315
Mateo Rodriguez <mateo@foursquare.com>
2015-12-30 18:29:07 +0000

    Allow FileSetWithSpec in validation.
    This was removed upstream but we use it for LessCss. If this release
    woprks otherwise, add a TODO to update LessCss to use the new API.

--- src/python/pants/base/validation.py
+++ src/python/pants/base/validation.py
@@ -8,10 +8,12 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 from six import string_types
 from twitter.common.collections import OrderedSet
 from twitter.common.dirutil.fileset import Fileset
+from pants.source.wrapped_globs import FilesetWithSpec
 
 
 def assert_list(obj, expected_type=string_types, can_be_none=True, default=(), key_arg=None,
-    allowable=(list, Fileset, OrderedSet, set, tuple), raise_type=ValueError):
+    allowable=(list, Fileset, FilesetWithSpec, OrderedSet, set, tuple), raise_type=ValueError):
+
   """
   This function is used to ensure that parameters set by users in BUILD files are of acceptable types.
   :param obj           : the object that may be a list. It will pass if it is of type in allowable.
