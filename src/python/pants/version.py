47c55d4cb26b31412f8f7a35163908a22f6c47c6
Patrick Lawson <pl@foursquare.com>
2016-06-21 11:55:29 -0400

    Bump version for 4sq fork.

--- src/python/pants/version.py
+++ src/python/pants/version.py
@@ -8,6 +8,6 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 from pants.base.revision import Revision
 
 
-VERSION = '1.0.1'
+VERSION = '1.0.1+fsq20160621155826'
 
 PANTS_SEMVER = Revision.semver(VERSION)
