16e928fd63f8196f2e2e04d455499d5a9d480d9e
Mateo Rodriguez <mateo@foursquare.com>
2017-09-04 10:58:20 +0000

    cut a 4sq release

---
+++ src/rust/engine/src/cffi/native_engine.sh
@@ -0,0 +1,1 @@
+export CFLAGS="-fno-strict-aliasing -fno-common -dynamic -DNDEBUG -g -fwrapv -O3 -Wall -Wstrict-prototypes -I/Users/mateo/dev/pants/build-support/pants_dev_deps.venv/include/python2.7"
