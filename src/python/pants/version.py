2bf7a58f8065d9f5be0603d6a5bb600cd6b52600
Mateo Rodriguez <mateo@foursquare.com>
2017-09-08 10:38:45 +0000

    new release to test

--- src/python/pants/version.py
+++ src/python/pants/version.py
@@ -8,6 +8,6 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 from packaging.version import Version
 
 
-VERSION = '1.2.1+fs1b'
+VERSION = '1.2.1+fs1c'
 
 PANTS_SEMVER = Version(VERSION)
